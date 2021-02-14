b1fc7e51e5e2ded852ff9b9e0f163667714f2d26
Abhishek Kumar <abhishek022kk@gmail.com>
2021-02-14 09:50:42 +0530

    initial login screen

--- frontend/App.js
+++ frontend/App.js
@@ -1,28 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import React, {useEffect} from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import AddStudyTask from './AddStudyTask'
-import StudyTaskList from './StudyTaskList';
-import axios from 'axios';
+import { StatusBar } from "expo-status-bar";
+import React, { useEffect } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import AddStudyTask from "./AddStudyTask";
+import StudyTaskList from "./StudyTaskList";
+import axios from "axios";
+import WelcomeScreen from "./components/WelcomeScreen";
 
 export default function App() {
-
   useEffect(() => {
-    const resp = axios.get(`http://rk2357.pythonanywhere.com/tasks`,
-    {
+    const resp = axios.get(`http://rk2357.pythonanywhere.com/tasks`, {
       params: {
         id: 1,
-        date: "2021-02-13"
-      }
-    }) 
+        date: "2021-02-13",
+      },
+    });
     console.log(resp.data);
   });
 
   return (
     <View style={styles.container}>
-      {/* <AddStudyTask /> */}
+      {/* <AddStudyTask />
       <StudyTaskList />
-      <StatusBar style="auto" />
+      <StatusBar style="auto" /> */}
+      <WelcomeScreen />
     </View>
   );
 }
@@ -30,7 +30,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    marginTop: 20
+    backgroundColor: "#fff",
+    marginTop: 20,
   },
 });
